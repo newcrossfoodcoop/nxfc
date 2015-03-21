c031ca7adfa0c603a70c5aba2ade83c5cde7d79e
Ben Simpson <ben@hy-street.net>
2015-03-21 07:53:14 +0000

    point environments to NXFC_MONGO

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://' + (process.env.MONGO_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+	db: 'mongodb://' + (process.env.NXFC_MONGO_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
 	app: {
 		title: 'NXFC - Development Environment'
 	},
