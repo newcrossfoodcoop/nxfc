2fb2748a06c7363ae71ca9cdd7545e89e42d603e
Ben Simpson <ben@hy-street.net>
2015-05-12 15:35:47 +0100

    + correct production configuration for ghost + make sure tests also do some linting

--- modules/ghost/server/content/config/ghost.server.content.config.js
+++ modules/ghost/server/content/config/ghost.server.content.config.js
@@ -10,7 +10,7 @@ var config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://app.newcrossfoodcoop.org.uk',
+        url: 'http://app.newcrossfoodcoop.org.uk' + moduleConfig.subdir,
         mail: {},
         database: {
             client: 'sqlite3',
