ed3c6b7a556f20263f2d6e9070ff7449b84ac9f6
Ben Simpson <ben@hy-street.net>
2015-05-23 00:33:47 +0100

    bad AND... replace & with &&

--- modules/home/server/config/home.server.config.js
+++ modules/home/server/config/home.server.config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var config = {
-    useHolding: process.env.HOLDING_USERNAME & process.env.HOLDING_PASSWORD,
+    useHolding: !!(process.env.HOLDING_USERNAME && process.env.HOLDING_PASSWORD),
     holdingUsername: process.env.HOLDING_USERNAME || 'HOLDING_USERNAME',
     holdingPassword: process.env.HOLDING_PASSWORD || 'HOLDING_PASSWORD'
 };
@@ -10,8 +10,9 @@ module.exports = function(app, db) {
 	// Root routing
 	var core = require('../controllers/home.server.controller'), 
 	    express = require('express');
-	
+
 	if (config.useHolding) {
+	    console.log('setting holding');
         app.use(express.static('../views/holding'));
         app.use(core.renderHoldingPage);
 
